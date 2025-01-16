f78f51e57933a65f775b606472c44a5e4e21bdda
Jhovana <jhovanapatty95@gmail.com>
2025-01-16 08:58:34 -0800

    se agrego ejercicios, mas operaciones aritmeticas, datos, series, vector, notas.

--- codigo.js
+++ codigo.js
@@ -23,6 +23,44 @@ function multiplicar(){
 
 // completar division y resta , raiz cuadrada . si un numero es par o impar
 
+function dividir(){
+    let num = parseInt(prompt("ingrese un numero"));
+    let num2 = parseInt(prompt("ingrese otro numero"));
+    let resultado = num / num2;
+    alert(resultado)
+}
+function resta(){
+    let num = parseInt(prompt("ingrese el numero"));
+    let num2 = parseInt(prompt("ingrese un segundo numero"));
+    let resultado = num - num2;
+
+    alert(resultado)
+}
+
+function raiz(){
+    let num = parseInt(prompt("ingrese numero del cual quiere su raicita cuadradita ;)"));
+    let resultado = Math.sqrt(num)
+
+    alert(resultado)
+}
+function numeros(){
+    let num = parseInt(prompt("ingrese un numero"));
+        if(num %2 == 0){
+            document.write("es par")
+        } else{
+            alert("es impar")
+        }
+}
+// incrementaer potencia
+ function potencia(){
+    let num = parseInt(prompt("ingrese numero "));
+    let potencia = parseInt(prompt("ingrese la cantidad a elevar"))
+    let resultado = num**potencia;
+
+    alert(resultado);
+ }
+
+
 
 // let numero = 10;
 // numero += 5;
@@ -70,3 +108,121 @@ function nombres(){
         alert("no es valido")
     }
 }
+
+// bucles (for)
+
+function For(){
+    let num = parseInt(prompt("ingrese su numero"));
+    let cont = 0;
+    for(let i = 1; i <= num; i++){
+        cont = cont + 1;
+        alert(cont);
+    }
+    
+}
+
+function datos(){
+    let num = parseInt(prompt("ingrese numero de la cantidad de veces"));
+    for(let i = 1; i<=num; i++){
+        prompt("ingrese su numero")
+
+    }
+}
+function serie(){
+    let num = parseInt(prompt("ingrese n"));
+    let serie = 5;
+    for(let i = 1; i<= num; i++){
+        alert(serie);
+        serie = serie + 5;
+    }
+        
+}
+function serieDe9(){
+    let n= parseInt(prompt("ingrece numero de veces"));
+    let series = 9;
+    for(let i = 1; i<= n; i++){
+        alert(series);
+        series= series + 9;   
+    }
+}
+
+// la funcion seeries jija
+// Función para generar las dos series
+function generarSeries(n) {
+    let serie1 = [];
+    let serie2 = [];
+  
+    for (let i = 0; i < n; i++) {
+      serie1[i] = i + 1; // Serie 1: Incrementa de 1 en 1
+      serie2[i] = 0;     // Serie 2: Siempre 0
+    }
+  
+    return { serie1, serie2 };
+  }
+  
+  // Generar las series
+  const n = 10; // Número de términos
+  const { serie1, serie2 } = generarSeries(n);
+  
+  // Imprimir las series en la página web
+  document.write("Serie 1: " + serie1.join(", ") + "<br>");
+  document.write("Serie 2: " + serie2.join(", "));
+
+function vector(){
+    let frutas= ["kiwi" , "tomate" , "manzana"];
+    alert(frutas)
+}
+
+function otraserie(){
+    let n = parseInt(prompt("Ingrese el valor de n: "));
+let serie = 1; 
+
+for (let i = 1; i <= n; i++) {
+    if (serie%2=== 0) {
+        document.write("0");
+        serie = 1; // 
+    } else {
+        document.write("1");
+        serie= 0; 
+    }
+}
+
+}
+function paresImpares(){
+    let n = parseInt(prompt("Ingrese el número de términos: "));
+let a = 3; 
+let b = 2; 
+
+for (let i = 1; i <= n; i++) {
+    if (i % 2 !== 0) { // ! es para  impar en condicional xd
+        document.write(a + ",");
+        a =a + 3; 
+    } else { 
+        document.write(b + ",");
+        b = b + 2; 
+    }
+}
+}
+
+
+
+function notas() {
+    let notaEstudiante = parseInt(prompt("Ingrese la calificación del estudiante (0-100):"));
+
+  let calificacion;
+  
+  if (notaEstudiante >= 90) {
+    calificacion = "A";
+  } else if (notaEstudiante >= 80) {
+    calificacion = "B";
+  } else if (notaEstudiante >= 70) {
+    calificacion = "C";
+  } else if (notaEstudiante >= 60) {
+    calificacion = "D";
+  } else {
+    calificacion = "F";
+  }
+  
+  document.write(calificacion) ;
+}
+
